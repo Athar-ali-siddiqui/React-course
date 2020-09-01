e65760d9a1f7e6f5e7289193325d30295a6fe180
Athar-ali-siddiqui <65170037+Athar-ali-siddiqui@users.noreply.github.com>
2020-09-02 02:13:25 +0500

    Component part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,21 +2,28 @@ import React from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 import "./App.css";
 import { render } from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">
-              <h3>Ristorante Con Fusion</h3>
+            <NavbarBrand href="/" tag="h2">
+              Ristorante Con Fusion
             </NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
