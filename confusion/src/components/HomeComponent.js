ab06d3c6a3c25652b4a09c63f1a9eae2fb6acdb6
Athar-ali-siddiqui <65170037+Athar-ali-siddiqui@users.noreply.github.com>
2020-10-18 20:43:45 +0500

    Single Page Applications Part 1

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Card , CardBody ,CardText ,CardTitle ,CardSubtitle ,CardImg} from 'reactstrap';
+
+function RenderCard({item}){
+  console.log(item)
+  // item = item.item
+  return(
+    <Card>
+      <CardImg src={item.image} alt={item.name}/>
+      <CardBody>
+      <CardTitle tag="h3">{item.name}</CardTitle>
+      {item.designation ? <CardSubtitle tag="h5">{item.designation}</CardSubtitle>:null}
+      <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  )
+}
 
 function Home(props) {
+  console.log(props)
   return(
     <div className="container">
-      <h1>Home</h1>
+      <div className="row my-2">
+        <div className="col-12 col-md-4">
+          <RenderCard item={props.dish}/>
+        </div>
+        <div className="col-12 col-md-4">
+          <RenderCard item={props.promotion}/>
+        </div>
+        <div className="col-12 col-md-4">
+          <RenderCard item={props.leader}/>
+        </div>
+      </div>
     </div>
   )
   
